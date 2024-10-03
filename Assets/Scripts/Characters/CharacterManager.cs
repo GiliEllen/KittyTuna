fc07235f054997a94bbada00ab054df8b8d2c249
gili <gilimena95@gmail.com>
2024-10-03 19:09:31 +0300

    chore: make the camera follow the cats

--- Assets/Scripts/Characters/CharacterManager.cs
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using Cinemachine;
 
 public class CharacterManager : MonoBehaviour
 {
     public PlayableCharacter[] characters; 
     private int currentCharacterIndex = 0;
     private PlayableCharacter currentCharacterInstance;
+    private CinemachineVirtualCamera virtualCamera;
 
     private void Start()
     {
         currentCharacterIndex = Random.Range(0, characters.Length);
-         Debug.Log($"Selected Character Index: {currentCharacterIndex}");
+        Debug.Log($"Selected Character Index: {currentCharacterIndex}");
+        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
         SpawnCharacter(currentCharacterIndex, transform.position);
     }
 
@@ -37,5 +40,6 @@ public class CharacterManager : MonoBehaviour
     {
         Debug.Log($"Spawning Character: {characters[index].name}");
         currentCharacterInstance = Instantiate(characters[index], spawnPosition, Quaternion.identity);
+        virtualCamera.Follow = currentCharacterInstance.transform;
     }
 }
